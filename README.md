4327569a7b381cb6d9c7e96c46455a393f4daa0a
Mateo Cerquetella <mateocerquetella@hotmail.com>
2020-09-18 22:47:31 -0300

    IMPROVE, yarn

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-
-
-
 # 🌎 Ayudalo App Api | 2020
 
 ### Install
 
-#### `yarn install express`
+#### `yarn add express`
+
 Global dependecies
 
-#### `yarn install @types/node typescript rimraf ts-node nodemon --save-dev`
+#### `yarn add -D @types/node @types/express typescript rimraf ts-node nodemon`
+
 Developer local dependecies
 
 ### Scripts
@@ -20,7 +19,3 @@ Starts the application in development using `nodemon` and `ts-node` to do hot re
 #### `yarn run build`
 
 Builds the app at `build`, cleaning the folder first.
-
-#### `yarn run start`
-
-Starts the app in production by first building the project with `npm run build`, and then executing the compiled JavaScript at `build/index.js`.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-function foo(bar: String) {
-    console.log(bar);
-}
+'use strict'
+import express from 'express';
 
-foo("hola mundo")
+const app = express();
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+
+// db.connect()
+//     .then(obj => {
+//         obj.done(); //Cargó la base de datos
+//         console.log('DB initiated...');
+//     })
+//     .catch(error => {
+//         console.log('DB Error:', error.message || error);
+//     });
+
+// usuariosRoutes(app);
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log('server on port:', app.get('port'));
+});
