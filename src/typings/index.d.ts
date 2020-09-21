4a84ecf5b69739304249f963ce6b1721e1d66723
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-09-21 11:44:36 -0300

    ADD, types

---
+++ src/typings/index.d.ts
@@ -0,0 +1,14 @@
+declare module NodeJS {
+  interface Global {
+    utils: {
+      parseBool: (txt: any) => boolean
+    };
+    CONSTANTS: {
+      DATABASE_URL: string;
+      DATABASE_PASSWORD: string;
+      DATABASE_PORT: string;
+      PRODUCTION: boolean;
+      PRODUCTION_DB: boolean;
+    };
+  }
+}
