bec7c52fe3e64c31a00f17e42b920e1f9c482059
Mateo Cerquetella <mateocerquetella@hotmail.com>
2020-09-26 13:43:18 -0300

    REMOVE, config

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,15 +1,15 @@
-declare module NodeJS {
-  interface Global {
-    utils: {
-      parseBool: (txt: any) => boolean;
-    };
-    CONSTANTS: {
-      DATABASE_HOST: string;
-      DATABASE_PASSWORD: string;
-      DATABASE_NAME: string;
-      DATABASE_USER: string;
-      PRODUCTION: boolean;
-      PRODUCTION_DB: boolean;
-    };
-  }
-}
+// declare module NodeJS {
+//   interface Global {
+//     utils: {
+//       parseBool: (txt: any) => boolean;
+//     };
+//     CONSTANTS: {
+//       DATABASE_HOST: string;
+//       DATABASE_PASSWORD: string;
+//       DATABASE_NAME: string;
+//       DATABASE_USER: string;
+//       PRODUCTION: boolean;
+//       PRODUCTION_DB: boolean;
+//     };
+//   }
+// }
