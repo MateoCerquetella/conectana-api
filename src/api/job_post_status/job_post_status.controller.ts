327ea845f77467d59b7f61aac87a2ebf6864587c
Mateo Cerquetella <mateocerquetella@hotmail.com>
2020-10-03 23:04:00 -0300

    FIX, job_post_status

--- src/api/job_post_status/job_post_status.controller.ts
+++ src/api/job_post_status/job_post_status.controller.ts
@@ -1,104 +1,104 @@
-import * as express from 'express';
-import db from '../../database/db';
-import { IJobPostStatus } from './job_post_status.model';
+import * as express from 'express'
+import db from '../../database/db'
+import { IJobPostStatus } from './job_post_status.model'
 
-const table = () => db<IJobPostStatus>('job_post_status');
+const table = () => db<IJobPostStatus>('jobPostStatus')
 
 export class JobPostStatusController {
   create(req: express.Request, res: express.Response) {
-    const job_post_statusTmp: IJobPostStatus = req.body;
+    const jobPostStatusTmp: IJobPostStatus = req.body
 
     //Validate request
-    if (!job_post_statusTmp.name) {
+    if (!jobPostStatusTmp.name) {
       return res.status(400).send({
         message: 'Falta contenido y/o no puede estar vacio.',
-      });
+      })
     }
 
     table()
-      .insert(job_post_statusTmp)
+      .insert(jobPostStatusTmp)
       .then(() => {
-        return res.status(200).send({ message: 'Creado con éxito' });
+        return res.status(200).send({ message: 'Creado con éxito' })
       })
       .catch((error) => {
         if (error.code === '23505') {
-          return res.status(409).send({ message: 'Ya existe el job post status' });
+          return res.status(409).send({ message: 'Ya existe el job post status' })
         }
         return res
           .status(500)
-          .json({ message: 'Server error', messageError: error.detail });
-      });
+          .json({ message: 'Server error', messageError: error.detail })
+      })
   }
 
   findAll(req: express.Request, res: express.Response) {
     table()
       .select()
-      .then((job_post_status: IJobPostStatus[]) => {
-        return res.status(200).send(job_post_status);
+      .then((jobPostStatus: IJobPostStatus[]) => {
+        return res.status(200).send(jobPostStatus)
       })
       .catch((error) => {
         return res
           .status(500)
-          .json({ message: 'Server error', messageError: error.detail });
-      });
+          .json({ message: 'Server error', messageError: error.detail })
+      })
   }
 
   findOne(req: express.Request, res: express.Response) {
-    const job_post_statusTmp: IJobPostStatus = req.body;
-    job_post_statusTmp.id = +req.params.id;
+    const jobPostStatusTmp: IJobPostStatus = req.body
+    jobPostStatusTmp.id = +req.params.id
 
     table()
-      .where({ id: job_post_statusTmp.id })
-      .then((job_post_status: IJobPostStatus[]) => {
-        return job_post_status.length > 0
-          ? res.status(200).send(job_post_status)
-          : res.status(404).send({ message: 'Job post status no encontrado' });
+      .where({ id: jobPostStatusTmp.id })
+      .then((jobPostStatus: IJobPostStatus[]) => {
+        return jobPostStatus.length > 0
+          ? res.status(200).send(jobPostStatus)
+          : res.status(404).send({ message: 'Job post status no encontrado' })
       })
       .catch((error) => {
         return res
           .status(500)
-          .json({ message: 'Server error', messageError: error.detail });
-      });
+          .json({ message: 'Server error', messageError: error.detail })
+      })
   }
 
   update(req: express.Request, res: express.Response) {
-    const job_post_statusTmp: IJobPostStatus = req.body;
+    const jobPostStatusTmp: IJobPostStatus = req.body
 
     //Validate request
-    if (!job_post_statusTmp.name) {
+    if (!jobPostStatusTmp.name) {
       return res.status(400).send({
         message: 'Falta contenido y/o no puede estar vacio.',
-      });
+      })
     }
 
     table()
       .where({ id: +req.params.id })
-      .update({ name: job_post_statusTmp.name })
-      .then((job_post_status: number) => {
-        return job_post_status > 0
+      .update({ name: jobPostStatusTmp.name })
+      .then((jobPostStatus: number) => {
+        return jobPostStatus > 0
           ? res.status(200).send({ message: 'Modificado con éxito' })
-          : res.status(404).send({ message: 'Job post status no encontrado' });
+          : res.status(404).send({ message: 'Job post status no encontrado' })
       })
       .catch((error) => {
         return res
           .status(500)
-          .json({ message: 'Server error', messageError: error.detail });
-      });
+          .json({ message: 'Server error', messageError: error.detail })
+      })
   }
 
   delete(req: express.Request, res: express.Response) {
     table()
       .where({ id: +req.params.id })
       .del()
-      .then((job_post_status: number) => {
-        return job_post_status > 0
+      .then((jobPostStatus: number) => {
+        return jobPostStatus > 0
           ? res.status(200).send({ message: 'Borrado con éxito' })
-          : res.status(404).send({ message: 'Job post status no encontrado' });
+          : res.status(404).send({ message: 'Job post status no encontrado' })
       })
       .catch((error) => {
         return res
           .status(500)
-          .json({ message: 'Server error', messageError: error.detail });
-      });
+          .json({ message: 'Server error', messageError: error.detail })
+      })
   }
 }
--- src/api/job_post_status/job_post_status.routes.ts
+++ src/api/job_post_status/job_post_status.routes.ts
@@ -1,17 +1,17 @@
-import express from 'express';
-import { Express } from 'express-serve-static-core';
-import { JobPostStatusController } from './job_post_status.controller';
-import { ensureAuthenticated } from '../middleware/auth';
+import express from 'express'
+import { Express } from 'express-serve-static-core'
+import { JobPostStatusController } from './job_post_status.controller'
+import { ensureAuthenticated } from '../middleware/auth'
 
-const router = express.Router();
-const job_post_status = new JobPostStatusController();
+const router = express.Router()
+const jobPostStatus = new JobPostStatusController()
 
 export default function jobPostStatusRoutes(app: Express) {
-    router.post('/', job_post_status.create);
-    router.get('/', job_post_status.findAll);
-    router.get('/:id', job_post_status.findOne);
-    router.put('/:id', job_post_status.update)
-    router.delete('/:id', job_post_status.delete);
+    router.post('/', ensureAuthenticated, jobPostStatus.create)
+    router.get('/', jobPostStatus.findAll)
+    router.get('/:id', jobPostStatus.findOne)
+    router.put('/:id', ensureAuthenticated, jobPostStatus.update)
+    router.delete('/:id', ensureAuthenticated, jobPostStatus.delete)
 
-    app.use('/job_post_status', router);
-};
+    app.use('/jobPostStatus', router)
+}
